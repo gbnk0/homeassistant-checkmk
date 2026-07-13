fb74933375331380c5a94f7f7eb6952991e6c954
gbnk0 <6898551+gbnk0@users.noreply.github.com>
2026-07-14 00:29:47 +0200

    Fix HACS and Hassfest validation

--- custom_components/homeassistant_checkmk/__init__.py
+++ custom_components/homeassistant_checkmk/__init__.py
@@ -2,10 +2,6 @@ from .config_flow import CheckmkOptionsFlowHandler
 def async_get_options_flow(config_entry):
     return CheckmkOptionsFlowHandler(config_entry)
 """Init for Checkmk integration with config flow"""
-from .const import DOMAIN
-
-async def async_setup(hass, config):
-    return True
 
 async def async_setup_entry(hass, entry):
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
